1696fdf5665fe1c17b52e0c1b3d1b305e4ea3bf1
Abalogu <wabalogu@gmail.com>
2025-09-09 01:07:01 +0100

    minor

--- components/ClientLayout.tsx
+++ components/ClientLayout.tsx
@@ -1,63 +1,24 @@
-
+// components/ClientLayout.tsx
 "use client";
 import Providers from '../app/providers';
 import { ModalProvider } from '@/contexts/ModalContext';
 import ModalManager from '@/components/modals/ModalManager';
-import { Suspense, useEffect, useState } from 'react';
+import { Suspense } from 'react';
+import PageLoader from './PageLoader';
 import { Toaster } from "@/components/ui/sonner";
 import '@/styles/globals.css';
-import Preloader from '@/components/Preloader';
-import { MouseTracker } from './ui/Tilt';
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {  
-  const [isLoading, setIsLoading] = useState(true);
-  const [finishLoading, setFinishLoading] = useState(false);
-
-  useEffect(() => {
-    
-    const timer = setTimeout(() => {
-      setFinishLoading(true); 
-      
-      
-      setTimeout(() => {
-        setIsLoading(false);
-      }, 700); 
-    }, 2000); 
-
-    return () => clearTimeout(timer);
-  }, []);
-
- return (
-  <Providers>
-    <ModalProvider>        
-      <Suspense fallback={null}>
-        {isLoading && (
-          <Preloader 
-            finishLoading={finishLoading}
-            delay={500}           
-            fadeDuration={200}    
-            color1="#000"         
-            color2="#ccc"         
-          />
-        )}
-      </Suspense>     
-
-      {/* Global Cursor */}
-      <MouseTracker 
-        enabled={true}         
-        cursorSize={10} 
-        followerSize={40} 
-        cursorColor="#000" 
-        followerColor="rgba(0, 0, 0, 0.42)" 
-        hoverElements="a, button, [data-hover]" 
-        minWidth={768} 
-      />
-
-      {children}       
-      <Toaster /> 
-      <ModalManager />
-    </ModalProvider>
-  </Providers>
-);
-
+  return (
+     <Providers>
+      <ModalProvider>        
+        <Suspense fallback={null}>
+            <PageLoader />
+        </Suspense>        
+        {children}       
+         <Toaster /> 
+        <ModalManager />
+      </ModalProvider>
+    </Providers>
+  );
 }
