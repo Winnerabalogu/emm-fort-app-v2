6c387b4c22a925e9702d52a3505587743875171c
Winnerabalogu <wabalogu@gmail.com>
2025-11-12 14:28:28 +0100

    minor

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,12 +1,15 @@
-"use client"
+"use client";
+
+import { Suspense } from "react";
 import LoginForm from "@/components/auth/LoginForm";
 import AuthLayout from "@/components/auth/AuthLayout";
 
-export default function LoginPage() {  
-
-  return (  
+export default function LoginPage() {
+  return (
     <AuthLayout>
-      <LoginForm />    
-      </AuthLayout> 
+      <Suspense fallback={<div>Loading...</div>}>
+        <LoginForm />
+      </Suspense>
+    </AuthLayout>
   );
 }
